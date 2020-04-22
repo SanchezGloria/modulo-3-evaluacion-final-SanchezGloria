c4ebacd923507c5775c75e5c6e2d033471b47c6d
SanchezGloria <gloriasancheztrillo@gmail.com>
2020-04-22 16:39:46 +0200

    :construction: Read data from API

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { fetchResults } from '../Service/ServiceResults';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      resultStore: [],
+    };
+  }
+
+  fetchNewResults() {
+    fetchResults().then((data) => {
+      this.setState({
+        resultStore: data,
+      });
+      console.log(this.state.resultStore);
+    });
+  }
+
+  componentDidMount() {
+    this.fetchNewResults();
   }
 
   render() {
