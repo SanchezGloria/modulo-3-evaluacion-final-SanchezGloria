aceac22f664a0af8dbb99656b150c21241139445
SanchezGloria <gloriasancheztrillo@gmail.com>
2020-04-25 13:10:35 +0200

    Update docs

---
+++ docs/precache-manifest.1ee20cf79b5b4104f5dba187a2c70f3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3b6eb67e999c6c3a79efa1cc30d957",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d29672519c91cc6f5fbb",
+    "url": "./static/css/main.8ac735ed.chunk.css"
+  },
+  {
+    "revision": "e73616be2f136c8a32e2",
+    "url": "./static/js/2.2fe5aed7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2fe5aed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29672519c91cc6f5fbb",
+    "url": "./static/js/main.eb5a99f5.chunk.js"
+  },
+  {
+    "revision": "41c2f3c96e512543836a",
+    "url": "./static/js/runtime-main.8bab44d2.js"
+  }
+]);
